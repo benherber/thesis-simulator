f301faf5e631c6d2dcfbea5f51a7a1ea4707dace
benherber <69871872+benherber@users.noreply.github.com>
2022-03-28 22:18:09 -0400

    fixed spacing

--- test/BerPlotterConstants.m
+++ test/BerPlotterConstants.m
@@ -4,13 +4,13 @@ function [params, total_symbs] = BerPlotterConstants(num_symbs, modulation_order
         modulation_order double {mustBePositive} = 2;
     end
 
-    total_symbs = 1e6;
+    total_symbs = 600;
 
     params = bherber.thesis.SimulationConstants( ...
                 2.4e9, ...   "Fc"
                 100e3, ...   "Fb_base"
                 100e3, ...   "Fb_step"
-                1 / 2 * (1 / (4 * 2.4e9)), ...   "Fb_channel_spacing"
+                0.5 / (1 / (4 * 2.4e9)), ...   "Fb_channel_spacing"
                 5, ...       "num_channels"
                 4, ...       "fs_granularity"
                 num_symbs, ... "num_symbs"
